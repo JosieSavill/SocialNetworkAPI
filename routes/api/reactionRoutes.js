e26a37b2a4ad05d67b8024511c062f44ce9903ad
JosieSavill <fiordasavilljosie@gmail.com>
2023-05-05 02:58:59 -0700

    added to reaction Routes file

---
+++ routes/api/reactionRoutes.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = express.Router();
+
+// import Reaction model
+const Reaction = require('../models/Reaction');
+
+// route to get all reactions
+router.get('/', async (req, res) => {
+
+    try {
+
+        cost reactions = await Reaction.find();
+        res.json(reactions);
+
+    } catch (err) {
+
+        res.status(500).json({ message: err.message });
+
+    }
+
+});
+
+// route to create a new reaction
+router.post('/', async (req, res) => {
+
+    const reaction = new Reaction({
+
+        type: req.body.type,
+        post_id: req.body.post_id,
+        user_id: req.body.user_id
+
+    });
+
+
+    try {
+
+        const newReaction = await reaction.save();
+        res.status(201).json(newReaction);
+
+    } catch (err) {
+
+        res.status(400).json({ message: err.message });
+
+    }
+
+    
+});
+
+
+// route to update an existin reacction
+router.patch('/:id', async (req, res) => {
+
+    try {
+
+        const reaction = await Reaction.findById(req.params.id);
+
+        if (req.body.type) {
+
+            reaction.type = req.body.type;
+
+        }
+
+        if (req.body.type) {
+
+            reaction.type = req.body.type;
+
+        }
+
+        if (req.body.post_id) {
+
+            reaction.post_id = req.body.post_id;
+        }
+
+        if (req.body.user_id) {
+
+            reaction.user_id = req.body.user_id;
+
+        }
+
+        const updateReaction = await reaction.save();
+        res.json(updatedReaction);
+
+    } catch (err) {
+
+        res.status(400).json({ message: err.message });
+
+    }
+
+});

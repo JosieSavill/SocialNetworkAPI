90d8ce26c17f7847072b1a6a63c31924da793f2e
JosieSavill <fiordasavilljosie@gmail.com>
2023-05-05 04:54:26 -0700

    fixed error on Thought model file

---
+++ models/Thought.js
@@ -0,0 +1,99 @@
+const { Schema model } = require('mongoose');
+const moment = require('moment');
+// const { kStringMaxLength } = require('buffer');
+
+const ThoughtSchema = new Schema(
+
+    {
+
+        thoughtText: {
+
+            type: String,
+            required:true,
+            minlength: 1,
+            maxlength: 280,
+
+        },
+
+        createdAt: {
+
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+
+        },
+
+        username: {
+
+            type: String,
+            required: true,
+
+        },
+
+        reactions:   [
+
+            {
+
+                reactionId: {
+
+                    type: Schema.Types.ObjectId,
+                    default: () => new Types.ObjectId(),
+
+                },
+
+                reactionBody: {
+
+                    type: String,
+                    required: true,
+                    maxlength: 280,
+
+                },
+
+                username: {
+
+                    type: String,
+                    required: true,
+
+                },
+
+                createdAt: {
+
+                    type: Date,
+                    default: Date.now,
+                    get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+
+                },
+
+            },
+
+        ],
+
+    },
+
+    {
+
+        toJSON: {
+
+            virtuals: true,
+            getters: true,
+
+        },
+
+        id: false,
+
+    }
+
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+
+    return this.reactions.length;
+
+});
+
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports - Thought;
+
+
